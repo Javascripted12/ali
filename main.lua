4c3490a465e4c5120b9a64087dd2fd0a6f146520
eltheartx26 <123979756+eltheartx26@users.noreply.github.com>
2023-01-30 19:17:36 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -61,7 +61,7 @@ local Toggle = Tab:CreateToggle({
 
 local Slider = Tab:CreateSlider({
     Name = "walkspeed",
-    Range = {0, 0},
+    Range = {0, 100},
     Increment = 1,
     Suffix = "Speed",
     CurrentValue = 5,
@@ -100,7 +100,7 @@ local Slider = Tab:CreateSlider({
 
  local Slider = Tab:CreateSlider({
     Name = "JumpPower",
-    Range = {0, 0},
+    Range = {0, 100},
     Increment = 1,
     Suffix = "Jump Power",
     CurrentValue = 5,
